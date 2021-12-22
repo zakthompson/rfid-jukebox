fd8616b0de64de13b23b88f7083a5a380506da8d
zakthompson <zak@thisfireinside.com>
2021-12-22 02:45:45 -0500

    Better sqlite3

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,8 +1,9 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('data.db');
+const sqlite3 = require('better-sqlite3');
+const db = new sqlite3('data.db', { verbose: console.log });
 
-db.run(`CREATE TABLE albums (
+const q = db.prepare(`CREATE TABLE albums (
   id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
   card_uid TEXT NOT NULL,
   album_id TEXT NOT NULL
 )`);
+q.run();
