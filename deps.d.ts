f58b5579e59965befb05f2948c8c31c36deeedd9
zakthompson <zak@thisfireinside.com>
2021-12-23 00:43:43 -0500

    Back to basics

--- deps.d.ts
+++
@@ -1,2 +0,0 @@
-declare module 'mfrc522-rpi';
-declare module 'rpi-softspi';
--- scripts/seed.js
+++
@@ -1,9 +0,0 @@
-const sqlite3 = require('better-sqlite3');
-const db = new sqlite3('data.db', { verbose: console.log });
-
-const q = db.prepare(`CREATE TABLE albums (
-  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  card_uid TEXT NOT NULL,
-  album_id TEXT NOT NULL
-)`);
-q.run();
