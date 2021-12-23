62537ad265141c52a9e6a4b6681037595e694982
zakthompson <zak@thisfireinside.com>
2021-12-23 14:59:44 -0500

    Setup script update to conserve space

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 rm -r /var/lib/mpd/music/albums
-cp -r /boot/albums /var/lib/mpd/music
-for file in /var/lib/mpd/music/albums/**/*' '* 
-do 
+mv /boot/albums /var/lib/mpd/music
+for file in /var/lib/mpd/music/albums/**/*' '*
+do
 	mv -- "$file" "${file// /_}"
 done
 chown -R mpd:audio /var/lib/mpd/music
+mkdir -p /boot/albums/01 /boot/albums/02 /boot/albums/03 /boot/albums/04 /boot/albums/05 /boot/albums/06 /boot/albums/07 /boot/albums/08 /boot/albums/09 /boot/albums/10
