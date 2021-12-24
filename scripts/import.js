076881401b5b1da6a47f2818010a5c573c9517e1
zakthompson <zak@thisfireinside.com>
2021-12-23 23:28:51 -0500

    New node import script - one album at a time

---
+++ scripts/import.js
@@ -0,0 +1,28 @@
+#!/home/pi/.nvm/versions/node/v17.3.0/bin/node
+
+const fs = require('fs');
+const { execSync } = require('child_process');
+
+let folderToImport;
+
+['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'].some((num) => {
+  const albumPath = `/boot/albums/${num}`;
+  const files = fs.readdirSync(albumPath);
+  if (files.length > 0) {
+    folderToImport = num;
+    return true;
+  }
+});
+
+fs.rmSync(`/var/lib/mpd/music/albums/${folderToImport}`, {
+  recursive: true,
+  force: true,
+});
+
+fs.renameSync(
+  `/boot/albums/${folderToImport}`,
+  `/var/lib/mpd/music/albums/${folderToImport}`
+);
+
+fs.mkdirSync(`/boot/albums/${folderToImport}`);
+execSync('chown -R mpd:audio /var/lib/mpd/music');
